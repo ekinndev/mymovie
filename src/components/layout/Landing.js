c45c854d32341b7773e5bd92353c1ce6005a0cab
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-10-01 23:14:24 +0300

    Oluşturulan sectionlara kart komponentleri eklendi

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import FilmCard from '../Card/FilmCard';
+import NewsCard from '../Card/NewsCard';
 
 function Landing({ siteTitle = 'My Movie', siteDesc = 'Sadece göründüğü gibi bir film sitesi değil...' }) {
   return (
-    <div className='landing'>
-      <div className='backdrop'>
-        <div className='site-info'>
-          <h1 className='large'>{siteTitle}</h1>
-          <p className='lead'>{siteDesc}</p>
+    <Fragment>
+      <div className='landing'>
+        <div className='backdrop'>
+          <div className='site-info'>
+            <h1 className='large'>{siteTitle}</h1>
+            <p className='lead'>{siteDesc}</p>
+          </div>
         </div>
       </div>
       <section className='last-films'>
-    </div>
+        <div className='container'>
+          <h1 className='large section-title'>Son Eklenen Filmler</h1>
+          <FilmCard />
+          <FilmCard />
+          <FilmCard />
+        </div>
       </section>
       <section className='last-news my-2'>
+        <div className='container'>
+          <h1 className='large section-title'>Son Eklenen Haberler</h1>
+
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
+        </div>
       </section>
+    </Fragment>
   );
 }
 
