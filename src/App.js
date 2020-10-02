2e61543b407a5fe041e38b5712f31871f8184865
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-10-02 10:04:38 +0300

    Store uygulamaya implemente edildi

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Layout from './components/layout/Layout';
 import NotFound from './components/layout/NotFound';
 import Filmler from './components/layout/Film/Films';
 import Haberler from './components/layout/News/News';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route exact path='/filmler' component={Filmler} />
-          <Route exact path='/haberler' component={Haberler} />
-          <Route exact path='/' component={Landing} />
-          <Route exact path='*' component={NotFound} />
-        </Switch>
-      </Layout>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path='/filmler' component={Filmler} />
+            <Route exact path='/haberler' component={Haberler} />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='*' component={NotFound} />
+          </Switch>
+        </Layout>
+      </Router>
+    </Provider>
   );
 }
 
