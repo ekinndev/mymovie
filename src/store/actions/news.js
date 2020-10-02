370e153fc4e0117b6040abe16680dd153d11431e
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-10-02 16:24:55 +0300

    Haber actionları fonksiyonları yazıldı

--- src/store/actions/news.js
+++ src/store/actions/news.js
@@ -3,13 +3,28 @@ import axios from 'axios';
 
 export const getNews = () => async dispatch => {
   try {
+    dispatch({ type: LOADING_START });
+
+    const { data } = await axios.get('/api/haberler');
+
+    dispatch({ type: GET_NEWS, payload: data });
   } catch (e) {
     console.log(e);
   }
 };
 
-export const getTheNews = () => async dispatch => {
+export const getTheNews = id => async dispatch => {
   try {
+    dispatch({ type: LOADING_START });
+    const { data } = await axios.get(`/api/haberler/${id}`);
+    dispatch({ type: GET_THE_NEWS, payload: data });
+  } catch (e) {
+    console.log(e);
+  }
+};
+export const clearNews = () => async dispatch => {
+  try {
+    dispatch({ type: CLEAR_THE_NEWS });
   } catch (e) {
     console.log(e);
   }
