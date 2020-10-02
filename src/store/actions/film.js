d7e62395709da3b02c7530ec7d5c2fc7d88ba9bc
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-10-02 13:56:03 +0300

    Get Film ve GetFilms actionları oluşturuldu

--- src/store/actions/film.js
+++ src/store/actions/film.js
@@ -3,13 +3,35 @@ import axios from 'axios';
 
 export const getFilms = () => async dispatch => {
   try {
+    dispatch({ type: LOADING_START });
+    const API_URL = `${process.env.REACT_APP_MOVIE_API}?apikey=${process.env.REACT_APP_KEY}`;
+    const res = await axios.get('/api');
+    const promises = res.data.map(async id => {
+      const queryUrl = `${API_URL}&i=${id}&plot=full`;
+      const filmResponse = await axios.get(queryUrl);
+      return filmResponse.data;
+    });
+    const films = await Promise.all(promises);
+    dispatch({ type: GET_FILMS, payload: films });
   } catch (e) {
     console.log(e);
   }
 };
 
-export const getFilm = () => async dispatch => {
+export const getFilm = id => async dispatch => {
   try {
+    dispatch({ type: LOADING_START });
+    const API_URL = `${process.env.REACT_APP_MOVIE_API}?apikey=${process.env.REACT_APP_KEY}`;
+    const queryUrl = `${API_URL}&i=${id}&plot=full`;
+    const film = await axios.get(queryUrl);
+    dispatch({ type: GET_FILM, payload: film.data });
+  } catch (e) {
+    console.log(e);
+  }
+};
+export const clearFilm = () => async dispatch => {
+  try {
+    dispatch({ type: CLEAR_FILM });
   } catch (e) {
     console.log(e);
   }
