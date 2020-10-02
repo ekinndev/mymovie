8e946ca33bd66aa68bdc7bdc9b95b65d0dca1017
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-10-02 09:17:36 +0300

    Haber ve Film Sayfaları oluşturuldu

---
+++ src/components/layout/Film/Films.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+function Films(props, films) {
+  return (
+    <Fragment>
+      <div className='container'>
+        <h1 className='large'>Filmler</h1>
+      </div>
+    </Fragment>
+  );
+}
+
+Films.propTypes = {};
+
+export default Films;
---
+++ src/components/layout/News/News.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+function News(props, News) {
+  return (
+    <Fragment>
+      <div className='container'>
+        <h1 className='large'>Haberler</h1>
+      </div>
+    </Fragment>
+  );
+}
+
+News.propTypes = {};
+
+export default News;
