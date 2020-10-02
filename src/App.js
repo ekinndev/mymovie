b56dca43f2524f2e2b5a9b21e7030160fddd26e9
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-10-02 09:18:17 +0300

    React router dom eklenip statik routelar oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Landing from './components/layout/Landing';
+import Layout from './components/layout/Layout';
+import NotFound from './components/layout/NotFound';
+import Filmler from './components/layout/Film/Films';
+import Haberler from './components/layout/News/News';
 
 function App() {
-  return <Landing />;
+  return (
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path='/filmler' component={Filmler} />
+          <Route exact path='/haberler' component={Haberler} />
+          <Route exact path='/' component={Landing} />
+          <Route exact path='*' component={NotFound} />
+        </Switch>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
